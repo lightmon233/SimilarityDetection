4530a67b231e2c62d977d213b4e30be3e207896a
Lightmon <3535888142@qq.com>
2024-08-28 20:59:09 +0800

    test cors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    Hello World!
+    通过后端取得：{{ msg }}
   </div>
   <router-view></router-view>
 </template>
 
 <script>
+
 import $ from 'jquery';
 import { ref } from 'vue';
 
 export default {
   name: "App",
   setup: () => {
-
+    let msg = ref("");
+    $.ajax({
+      url: "http://localhost:3000/",
+      type: "get", 
+      success: resp => {
+        msg.value = resp;
+      }
+    })
+    return {
+      msg
+    }
   }
 }
+
 </script>
 
 <style>
