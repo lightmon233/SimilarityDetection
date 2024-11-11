939fbd5ca70fac0aa1e12364e4132c2a05468fd2
lightmon <lightmon5210@outlook.com>
2024-11-11 09:30:35 +0800

    install and fixed monaco

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin'); 
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['javascript', 'typescript', 'css', 'html'],  // 指定要加载的语言
+        features: ['!gotoSymbol']  // 可选：禁用一些功能，减少打包体积
+      })
+    ]
+  },
+
+  // chainWebpack: config => {
+  //   config.resolve.alias
+  //     .set('monaco-editor', 'monaco-editor/esm/vs/editor/editor.api.js');  // Monaco 编辑器路径
+  // }
+}
